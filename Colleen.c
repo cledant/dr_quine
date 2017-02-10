5a9931fb0aa93a1c5c84fd2507dea4b83a64f551
cledant <cledant@student.42.fr>
2017-02-10 11:07:23 +0100

    updated Colleen

--- Colleen.c
+++ Colleen.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-void toto(void){
 /*
    Toto
-*/}
+*/
+void toto(void){}
 int main(void){
 /*
    Tata
 */
-char*a="#include<stdio.h>%cvoid toto(void){%c/*%c   Toto%c*/}%cint main(void){%c/*%c   Tata%c*/%cchar*a=%c%s%c;printf(a,10,10,10,10,10,10,10,10,10,34,a,34,10);}%c";printf(a,10,10,10,10,10,10,10,10,10,34,a,34,10);}
+char*a="#include<stdio.h>%c/*%c   Toto%c*/%cvoid toto(void){}%cint main(void){%c/*%c   Tata%c*/%cchar*a=%c%s%c;printf(a,10,10,10,10,10,10,10,10,10,34,a,34,10);}%c";printf(a,10,10,10,10,10,10,10,10,10,34,a,34,10);}
