cb7a6304250bd673b3fea8e2ba75437b7285e3dc
cledant <cledant@student.42.fr>
2017-02-11 17:27:46 +0100

    WIP SUlly

---
+++ Python/Colleen/Colleen.py
@@ -0,0 +1,4 @@
+#Toto
+def toto(): return None
+def main():a = '#Toto%cdef toto(): return None%cdef main():a = %r;%c%c%cTutu%c%c%c;print(a%%(10,10,a,39,39,39,39,39,39,10))%cif __name__ == "__main__": main()';'''Tutu''';print(a%(10,10,a,39,39,39,39,39,39,10))
+if __name__ == "__main__": main()
---
+++ Python/test3.py
@@ -0,0 +1,4 @@
+i = 5
+toto = open("tutu", "w")
+toto.write("TEST TEST TEST %d" % i)
+toto.close()
