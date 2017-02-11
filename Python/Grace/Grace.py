d77756c0a8c9ab09d59812418744cd83e7aa8597
cledant <cledant@student.42.fr>
2017-02-11 21:17:46 +0100

    Grace.py done

--- Python/Grace/Grace.py
+++ Python/Grace/Grace.py
@@ -1,4 +1,6 @@
 #Toto
-a=''
-b='tata'
-
+a='Grace_kid.py'
+b='#Toto%ca=%r%cb=%r%cc=%r%cdef titi():f = open(a, "w");f.write(b%%(10,a,10,b,10,c,10,10,10));f.close()%ctiti()%c'
+c='tutu'
+def titi():f = open(a, "w");f.write(b%(10,a,10,b,10,c,10,10,10));f.close()
+titi()
