fc8dac628b55190425068d956b274960edf3bf4b
cledant <cledant@student.42.fr>
2017-02-11 19:19:41 +0100

    Python Sully done

--- Python/Sully/Sully.py
+++ Python/Sully/Sully.py
@@ -1,10 +1,12 @@
 import sys
 i = int(5)
 if i == 0:sys.exit
-name = "Sully_"
-if sys.argv[0].find(name) != -1:i = i - 1
+name = "Sully.py"
+i = i - 1
+if sys.argv[0].find(name) == 0:i = 5
 f = open("Sully_%d.py" % i, "w")
-a = 'import sys%ci = int(%d)%cif i == 0:sys.exit%cname = "Sully_"%cif sys.argv[0].find(name) != -1:i = i - 1%cf = open("Sully_%%d.py" %% i, "w")%ca = %r%cf.write(a%%(10,i,10,10,10,10,10,a,10,10,10))%cf.close()%cif i != 0:execfile("Sully_%%d.py" %% i)'
-f.write(a%(10,i,10,10,10,10,10,a,10,10,10))
+a = 'import sys%ci = int(%d)%cif i == 0:sys.exit%cname = "Sully.py"%ci = i - 1%cif sys.argv[0].find(name) == 0:i = 5%cf = open("Sully_%%d.py" %% i, "w")%ca = %r%cf.write(a%%(10,i,10,10,10,10,10,10,a,10,10,10,10,10))%cf.close()%csys.argv[0] = ("Sully_%%d.py" %% i)%cif i != 0:exec(open("Sully_%%d.py" %% i, "r").read())%c'
+f.write(a%(10,i,10,10,10,10,10,10,a,10,10,10,10,10))
 f.close()
-if i != 0:execfile("Sully_%d.py" % i)
+sys.argv[0] = ("Sully_%d.py" % i)
+if i != 0:exec(open("Sully_%d.py" % i, "r").read())
